47d5c2ca6040ef37034759516bc113d988ae52ec
thep <theppitak@gmail.com>
2008-02-07 14:34:23 +0000

    * Check for autoconf/autoconf2.50 commands before calling the right 	one that's present.

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,18 @@
 
 set -x
 
-autoheader2.50
-autoconf2.50
+# autoconf2.50 is the diverted name when installed with autoconf2.13
+# at the same time on Debian. We prefer autoconf2.50.
+
+if autoheader2.50 --version </dev/null >/dev/null; then
+  autoheader2.50
+else
+  autoheader
+fi
+
+if autoconf2.50 --version </dev/null >/dev/null; then
+  autoconf2.50
+else
+  autoconf
+fi
 
